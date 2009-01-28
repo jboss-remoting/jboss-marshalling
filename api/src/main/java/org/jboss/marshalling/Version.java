36a972184147e239a8a0266a01d7416b70afd776
David M. Lloyd <david.lloyd@redhat.com>
2009-01-28 17:13:12 +0000

    Fix version

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.0.0.GA";
+    public static final String VERSION = "1.1.0.Alpha";
 
     /**
      * Print the version to {@code System.out}.

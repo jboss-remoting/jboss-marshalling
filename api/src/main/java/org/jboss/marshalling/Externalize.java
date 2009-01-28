8619711351283a1c95c94320f0e95a5e54888f3d
David M. Lloyd <david.lloyd@redhat.com>
2009-01-28 16:44:32 +0000

    JBMAR-12: Add API for class externalizers, deprecate instance externalizers (part 2)

---
+++ api/src/main/java/org/jboss/marshalling/Externalize.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.marshalling;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Documented;
+
+/**
+ * Indicate that this class should be externalized by an instance of the given externalizer class.  Any externalizer
+ * provided by a {@link org.jboss.marshalling.ClassExternalizerFactory ClassExternalizerFactory} or a
+ * {@link org.jboss.marshalling.ExternalizerFactory ExternalizerFactory} will take precedence over the externalizer
+ * specified by this annotation.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Inherited
+@Documented
+public @interface Externalize {
+
+    /**
+     * Specify the externalizer class to be used by the annotated class.
+     *
+     * @return the externalizer type
+     */
+    Class<? extends Externalizer> value();
+}

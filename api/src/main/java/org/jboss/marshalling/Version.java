4eaf07091b7b0db7b5bd37d1078f9de2727ded3f
David M. Lloyd <david.lloyd@redhat.com>
2009-02-25 01:47:38 +0000

    Actually, lets just make it CR1

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.1.0.Beta1";
+    public static final String VERSION = "1.1.0.CR1";
 
     /**
      * Print the version to {@code System.out}.

3783a9cf5610b1d13bb4056d78527d3b7368128b
David M. Lloyd <david.lloyd@redhat.com>
2009-05-19 16:48:35 +0000

    Next is 1.1.4-SNAPSHOT, I guess

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.1.3.GA";
+    public static final String VERSION = "1.1.4-SNAPSHOT";
 
     /**
      * Print the version to {@code System.out}.

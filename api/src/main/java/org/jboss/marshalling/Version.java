a78aeeeced93b386e0584d1b8bffb5fcba3b4179
David M. Lloyd <david.lloyd@redhat.com>
2009-06-01 17:20:56 +0000

    Next is 1.2.0-SNAPSHOT

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.2.0.CR1";
+    public static final String VERSION = "1.2.0-SNAPSHOT";
 
     /**
      * Print the version to {@code System.out}.

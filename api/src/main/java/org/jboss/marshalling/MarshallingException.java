73b5d1b18b027c47d38062ec0903a23bc41054ef
David M. Lloyd <david.lloyd@redhat.com>
2009-09-10 15:37:40 +0000

    Add a marshalling exception type

---
+++ api/src/main/java/org/jboss/marshalling/MarshallingException.java
@@ -0,0 +1,121 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.marshalling;
+
+import java.io.Serializable;
+
+/**
+ * A special {@code Throwable} which holds information about the cause of a marshalling problem.
+ */
+public final class MarshallingException extends Throwable {
+
+    private static final long serialVersionUID = 7163010203765763875L;
+
+    private Info info = null;
+
+    public static void addInformation(Throwable t, Object targetObject, String fieldName) {
+        if (t instanceof MarshallingException) {
+            final MarshallingException me = (MarshallingException) t;
+            final String targetClassName = targetObject.getClass().getName();
+            final int targetHashCode = targetObject.hashCode();
+            final Info oldInfo = me.info;
+            me.info = new Info(oldInfo, targetClassName, targetHashCode, fieldName);
+        } else {
+            Throwable c = t.getCause();
+            if (c == null) {
+                t.initCause(c = new MarshallingException());
+            }
+            addInformation(c, targetObject, fieldName);
+        }
+    }
+
+    public Throwable fillInStackTrace() {
+        // no operation
+        return this;
+    }
+
+    public String toString() {
+        final StringBuilder builder = new StringBuilder(256);
+        builder.append("Marshalling exception occurred:");
+        info.toString(builder);
+        return builder.toString();
+    }
+
+    public void setStackTrace(final StackTraceElement[] stackTrace) {
+        // nothing
+    }
+
+    /**
+     * Information of an object which was being marshalled at the time an exception occurred.
+     */
+    public static final class Info implements Serializable {
+
+        private static final long serialVersionUID = -8580895864558204394L;
+
+        private final Info cause;
+        private final String targetClassName;
+        private final int targetHashCode;
+        private final String targetFieldName;
+
+        public Info(final Info cause, final String targetClassName, final int targetHashCode, final String targetFieldName) {
+            this.cause = cause;
+            this.targetClassName = targetClassName;
+            this.targetHashCode = targetHashCode;
+            this.targetFieldName = targetFieldName;
+        }
+
+        public Info getCause() {
+            return cause;
+        }
+
+        public String getTargetClassName() {
+            return targetClassName;
+        }
+
+        public int getTargetHashCode() {
+            return targetHashCode;
+        }
+
+        public String getTargetFieldName() {
+            return targetFieldName;
+        }
+
+        public String toString() {
+            final StringBuilder builder = new StringBuilder(256);
+            toString(builder);
+            return builder.toString();
+        }
+
+        private void toString(StringBuilder builder) {
+            final Info cause = this.cause;
+            if (cause != null) {
+                cause.toString(builder);
+            }
+            builder.append("\n\tfrom object ").append(targetClassName).append('@').append(Integer.toHexString(targetHashCode));
+            final String fieldName = targetFieldName;
+            if (fieldName != null) {
+                builder.append(" (field ").append(fieldName).append(')');
+            }
+        }
+    }
+}

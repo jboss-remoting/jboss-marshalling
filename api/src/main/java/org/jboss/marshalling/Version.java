2ccdeb33f8537e09c8c2905c50e9f3e598802c5f
David M. Lloyd <david.lloyd@redhat.com>
2009-09-25 03:11:06 +0000

    Next will be 1.2.0.CR5 or GA

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.2.0.CR4";
+    public static final String VERSION = "1.2.0.CR5-SNAPSHOT";
 
     /**
      * Print the version to {@code System.out}.

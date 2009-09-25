21003302a2bd74327a302eb94adba904367044a1
David M. Lloyd <david.lloyd@redhat.com>
2009-09-25 03:08:04 +0000

    Prep 1.2.0.CR4 (2)

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.2.0-SNAPSHOT";
+    public static final String VERSION = "1.2.0.CR4";
 
     /**
      * Print the version to {@code System.out}.

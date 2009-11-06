aba7ad1ce36a058e8f383ccc59883e9b144298b2
David M. Lloyd <david.lloyd@redhat.com>
2009-11-06 23:08:45 +0000

    JBMAR-85 - NIO-friendly ByteInput/ByteOutput

---
+++ api/src/main/java/org/jboss/marshalling/NioByteInput.java
@@ -0,0 +1,232 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, JBoss Inc., and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.marshalling;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.nio.ByteBuffer;
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+/**
+ * A {@code ByteInput} implementation which is populated asynchronously with {@link ByteBuffer} instances.
+ */
+public class NioByteInput extends InputStream implements ByteInput {
+    private final Queue<ByteBuffer> queue;
+    private final Runnable ackTask;
+    private final Runnable closeTask;
+
+    // protected by "queue"
+    private boolean eof;
+    private IOException failure;
+
+    /**
+     * Construct a new instance.  The given {@code ackTask} will
+     * be invoked after each buffer is fully read.  The given {@code closeTask} will be called when the stream is closed.
+     *
+     * @param ackTask the read acknowledge task
+     * @param closeTask the close task
+     */
+    public NioByteInput(final Runnable ackTask, final Runnable closeTask) {
+        this.ackTask = ackTask;
+        this.closeTask = closeTask;
+        queue = new ArrayDeque<ByteBuffer>();
+    }
+
+    /**
+     * Push a buffer into the queue.  There is no mechanism to limit the number of pushed buffers; if such a mechanism
+     * is desired, it must be implemented externally, for example maybe using a {@link java.util.concurrent.Semaphore Semaphore}.
+     *
+     * @param buffer the buffer from which more data should be read
+     */
+    public void push(final ByteBuffer buffer) {
+        synchronized (this) {
+            if (!eof && failure == null) {
+                queue.add(buffer);
+            }
+        }
+    }
+
+    /**
+     * Push the EOF condition into the queue.  After this method is called, no further buffers may be pushed into this
+     * instance.
+     */
+    public void pushEof() {
+        synchronized (this) {
+            eof = true;
+        }
+    }
+
+    /**
+     * Push an exception condition into the queue.  After this method is called, no further buffers may be pushed into this
+     * instance.
+     *
+     * @param e the exception to push
+     */
+    public void pushException(IOException e) {
+        synchronized (this) {
+            if (! eof) {
+                failure = e;
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    public int read() throws IOException {
+        final Queue<ByteBuffer> queue = this.queue;
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                if (eof) {
+                    return -1;
+                }
+                checkFailure();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new InterruptedIOException("Interrupted on read()");
+                }
+            }
+            final ByteBuffer buf = queue.peek();
+            try {
+                return buf.get() & 0xff;
+            } finally {
+                if (buf.remaining() == 0) {
+                    queue.poll();
+                    ackTask.run();
+                }
+            }
+        }
+    }
+
+    /** {@inheritDoc} */
+    public int read(final byte[] b, final int off, int len) throws IOException {
+        if (len == 0) {
+            return 0;
+        }
+        final Queue<ByteBuffer> queue = this.queue;
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                if (eof) {
+                    return -1;
+                }
+                checkFailure();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new InterruptedIOException("Interrupted on read()");
+                }
+            }
+            int total = 0;
+            while (len > 0) {
+                final ByteBuffer buffer = queue.peek();
+                if (buffer == null) {
+                    break;
+                }
+                final int bytecnt = Math.min(buffer.remaining(), len);
+                buffer.get(b, off, bytecnt);
+                total += bytecnt;
+                len -= bytecnt;
+                if (buffer.remaining() == 0) {
+                    queue.poll();
+                    ackTask.run();
+                }
+            }
+            return total;
+        }
+    }
+
+    /** {@inheritDoc} */
+    public int available() throws IOException {
+        synchronized (this) {
+            int total = 0;
+            for (ByteBuffer buffer : queue) {
+                total += buffer.remaining();
+                if (total < 0) {
+                    return Integer.MAX_VALUE;
+                }
+            }
+            return total;
+        }
+    }
+
+    public long skip(long qty) throws IOException {
+        final Queue<ByteBuffer> queue = this.queue;
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                if (eof) {
+                    return 0L;
+                }
+                checkFailure();
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new InterruptedIOException("Interrupted on read()");
+                }
+            }
+            long skipped = 0L;
+            while (qty > 0L) {
+                final ByteBuffer buffer = queue.peek();
+                if (buffer == null) {
+                    break;
+                }
+                final int bytecnt = Math.min(buffer.remaining(), (int) Math.max((long)Integer.MAX_VALUE, qty));
+                buffer.position(buffer.position() + bytecnt);
+                skipped += bytecnt;
+                qty -= bytecnt;
+                if (buffer.remaining() == 0) {
+                    queue.poll();
+                    ackTask.run();
+                }
+            }
+            return skipped;
+        }
+    }
+
+    /** {@inheritDoc} */
+    public void close() throws IOException {
+        synchronized (this) {
+            if (! eof) {
+                queue.clear();
+                eof = true;
+                closeTask.run();
+            }
+        }
+    }
+
+    private void checkFailure() throws IOException {
+        final IOException failure = this.failure;
+        if (failure != null) {
+            failure.fillInStackTrace();
+            try {
+                throw failure;
+            } finally {
+                eof = true;
+                this.failure = null;
+            }
+        }
+    }
+}

31ca74db94916584c3184db0266bda79efdd4dcc
David M. Lloyd <david.lloyd@redhat.com>
2010-03-01 18:29:16 +0000

    Make API JAR executable; inject version from POM so that it is centrally managed

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,16 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.3.0.CR1-SNAPSHOT";
+    public static final String VERSION = getVersionString();
+
+    /**
+     * Get the version string.
+     *
+     * @return the version string
+     */
+    static String getVersionString() {
+        return "TRUNK SNAPSHOT";
+    }
 
     /**
      * Print the version to {@code System.out}.

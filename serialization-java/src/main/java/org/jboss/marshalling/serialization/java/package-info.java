2198c6771229bef4cbab2bf57ae07f6dd96d6648
David M. Lloyd <david.lloyd@redhat.com>
2010-03-01 17:42:47 +0000

    JMBAR-64: Remove reflection-based serialization impl

--- serialization-java/src/main/java/org/jboss/marshalling/serialization/java/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * The Java serialization-compatible protocol implementation package.  Construct an instance of the {@link org.jboss.marshalling.serialization.java.JavaSerializationMarshallerFactory JavaSerializationMarshallerFactory}
- * class to use this marshalling protocol.
- */
-package org.jboss.marshalling.serialization.java;

5703685595fbb798b9fcf2cf7e51c76e1118cd93
David M. Lloyd <david.lloyd@redhat.com>
2010-08-05 15:55:54 +0000

    Next is 1.2.3

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.2.2.GA";
+    public static final String VERSION = "1.2.3.CR1-SNAPSHOT";
 
     /**
      * Print the version to {@code System.out}.

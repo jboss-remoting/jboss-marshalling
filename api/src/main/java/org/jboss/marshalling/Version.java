25809b8db08ffb5477197d59441cab41b6eedd86
David M. Lloyd <david.lloyd@redhat.com>
2010-08-20 21:59:09 +0000

    Prep 1.2.3.GA

--- api/src/main/java/org/jboss/marshalling/Version.java
+++ api/src/main/java/org/jboss/marshalling/Version.java
@@ -33,7 +33,7 @@ public final class Version {
     /**
      * The version.
      */
-    public static final String VERSION = "1.2.3.CR1-SNAPSHOT";
+    public static final String VERSION = "1.2.3.GA";
 
     /**
      * Print the version to {@code System.out}.

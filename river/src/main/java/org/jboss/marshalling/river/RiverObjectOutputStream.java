f49854912769deb6504e9a327f354f62b9bdf4b3
David M. Lloyd <david.lloyd@redhat.com>
2014-02-14 13:04:31 -0600

    [JBMAR-120] When fields are not read/written, take the action of writing empty field values or reading and discarding values

--- river/src/main/java/org/jboss/marshalling/river/RiverObjectOutputStream.java
+++ river/src/main/java/org/jboss/marshalling/river/RiverObjectOutputStream.java
@@ -38,22 +38,18 @@ import org.jboss.marshalling.reflect.SerializableField;
 import org.jboss.marshalling.reflect.SerializableClass;
 import java.io.IOException;
 import java.io.NotActiveException;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  *
  */
 public class RiverObjectOutputStream extends MarshallerObjectOutputStream {
-    protected enum State {
-        OFF,
-        UNWRITTEN_FIELDS,
-        ON,
-        ;
-    }
+    private static final int OFF = 0;
+    private static final int UNWRITTEN_FIELDS = 1;
+    private static final int ON = 2;
 
-    private final AtomicReference<State> state = new AtomicReference<State>(State.OFF);
     private final RiverMarshaller marshaller;
 
+    private int state;
     private RiverPutField putField;
     private SerializableClass serializableClass;
     private Object current;
@@ -63,12 +59,28 @@ public class RiverObjectOutputStream extends MarshallerObjectOutputStream {
         this.marshaller = marshaller;
     }
 
+    private boolean compareAndSetState(int expect, int set) {
+        if (state == expect) {
+            state = set;
+            return true;
+        }
+        return false;
+    }
+
+    private int getAndSetState(int set) {
+        try {
+            return state;
+        } finally {
+            state = set;
+        }
+    }
+
     public void writeFields() throws IOException {
         final RiverPutField putField = this.putField;
         if (putField == null) {
             throw new NotActiveException("no current PutField object");
         }
-        if (! state.compareAndSet(State.UNWRITTEN_FIELDS, State.ON)) {
+        if (! compareAndSetState(UNWRITTEN_FIELDS, ON)) {
             throw new NotActiveException("writeFields() may only be called when the fields have not yet been written");
         }
         this.putField = null;
@@ -76,7 +88,7 @@ public class RiverObjectOutputStream extends MarshallerObjectOutputStream {
     }
 
     public PutField putFields() throws IOException {
-        if (state.get() == State.OFF) {
+        if (state == OFF) {
             throw new NotActiveException("No object is currently being serialized");
         }
         if (putField == null) {
@@ -150,7 +162,7 @@ public class RiverObjectOutputStream extends MarshallerObjectOutputStream {
     }
 
     public void defaultWriteObject() throws IOException {
-        if (! state.compareAndSet(State.UNWRITTEN_FIELDS, State.ON)) {
+        if (! compareAndSetState(UNWRITTEN_FIELDS, ON)) {
             throw new NotActiveException("writeFields() may only be called when the fields have not yet been written");
         }
         try {
@@ -162,21 +174,117 @@ public class RiverObjectOutputStream extends MarshallerObjectOutputStream {
         }
     }
 
-    protected State start() throws IOException {
-        return state.getAndSet(State.UNWRITTEN_FIELDS);
+    protected int start() throws IOException {
+        return getAndSetState(UNWRITTEN_FIELDS);
     }
 
-    protected void finish(State restoreState) throws IOException {
-        switch (state.getAndSet(restoreState)) {
-            case UNWRITTEN_FIELDS:
-            	if(serializableClass.getFields().length > 0) {
-            		throw new NotActiveException("Fields were never written");
-            	}
+    protected void writeObjectOverride(final Object obj) throws IOException {
+        checkState();
+        super.writeObjectOverride(obj);
+    }
+
+    public void writeUnshared(final Object obj) throws IOException {
+        checkState();
+        super.writeUnshared(obj);
+    }
+
+    public void write(final int val) throws IOException {
+        checkState();
+        super.write(val);
+    }
+
+    public void write(final byte[] buf) throws IOException {
+        checkState();
+        super.write(buf);
+    }
+
+    public void write(final byte[] buf, final int off, final int len) throws IOException {
+        checkState();
+        super.write(buf, off, len);
+    }
+
+    public void writeBoolean(final boolean val) throws IOException {
+        checkState();
+        super.writeBoolean(val);
+    }
+
+    public void writeByte(final int val) throws IOException {
+        checkState();
+        super.writeByte(val);
+    }
+
+    public void writeShort(final int val) throws IOException {
+        checkState();
+        super.writeShort(val);
+    }
+
+    public void writeChar(final int val) throws IOException {
+        checkState();
+        super.writeChar(val);
+    }
+
+    public void writeInt(final int val) throws IOException {
+        checkState();
+        super.writeInt(val);
+    }
+
+    public void writeLong(final long val) throws IOException {
+        checkState();
+        super.writeLong(val);
+    }
+
+    public void writeFloat(final float val) throws IOException {
+        checkState();
+        super.writeFloat(val);
+    }
+
+    public void writeDouble(final double val) throws IOException {
+        checkState();
+        super.writeDouble(val);
+    }
+
+    public void writeBytes(final String str) throws IOException {
+        checkState();
+        super.writeBytes(str);
+    }
+
+    public void writeChars(final String str) throws IOException {
+        checkState();
+        super.writeChars(str);
+    }
+
+    public void writeUTF(final String str) throws IOException {
+        checkState();
+        super.writeUTF(str);
+    }
+
+    protected void finish(int restoreState) throws IOException {
+        try {
+            if (state == UNWRITTEN_FIELDS) {
+                marshaller.doWriteEmptyFields(serializableClass);
+            }
+        } finally {
+            state = restoreState;
+        }
+    }
+
+    private void checkState() throws IOException {
+        int state = this.state;
+        if (state == ON) {
+            return;
+        } else if (state == OFF) {
+            throw new NotActiveException("Object stream not active");
+        } else if (state == UNWRITTEN_FIELDS) {
+            this.state = ON;
+            marshaller.doWriteEmptyFields(serializableClass);
+            return;
+        } else {
+            throw new IllegalStateException("Unknown state");
         }
     }
 
     protected void fullReset() {
-        state.set(State.OFF);
+        state = OFF;
         putField = null;
         serializableClass = null;
         current = null;

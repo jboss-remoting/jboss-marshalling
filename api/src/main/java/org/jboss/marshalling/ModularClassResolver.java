a8e62a97271e302c7ef8d66311cfb0992b18974c
Radoslav Husar <radosoft@gmail.com>
2016-01-11 01:57:00 +0100

    JBMAR-180 SecurityManager issue in ModularClassResolver#resolveClass and ModularClassResolver#resolveProxyClass

--- api/src/main/java/org/jboss/marshalling/ModularClassResolver.java
+++ api/src/main/java/org/jboss/marshalling/ModularClassResolver.java
@@ -21,6 +21,9 @@ package org.jboss.marshalling;
 import java.io.IOException;
 import java.io.InvalidClassException;
 import java.lang.reflect.Proxy;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 import org.jboss.modules.Module;
 import org.jboss.modules.ModuleIdentifier;
 import org.jboss.modules.ModuleLoadException;
@@ -94,12 +97,35 @@ public final class ModularClassResolver implements ClassResolver {
     public Class<?> resolveClass(final Unmarshaller unmarshaller, final String className, final long serialVersionUID) throws IOException, ClassNotFoundException {
         final String name = (String) unmarshaller.readObject();
         if (name == null) {
-            return Class.forName(className, false, Module.class.getClassLoader());
+            final ClassLoader classLoader;
+            if (System.getSecurityManager() == null) {
+                classLoader = Module.class.getClassLoader();
+            } else {
+                classLoader = AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
+                    @Override
+                    public ClassLoader run() {
+                        return Module.class.getClassLoader();
+                    }
+                });
+            }
+            return Class.forName(className, false, classLoader);
         }
         final String slot = (String) unmarshaller.readObject();
         final ModuleIdentifier identifier = ModuleIdentifier.create(name, slot);
         try {
-            return Class.forName(className, false, moduleLoader.loadModule(identifier).getClassLoader());
+            final ClassLoader classLoader;
+            final Module module = moduleLoader.loadModule(identifier);
+            if (System.getSecurityManager() == null) {
+                classLoader = module.getClassLoader();
+            } else {
+                classLoader = AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
+                    @Override
+                    public ClassLoader run() {
+                        return module.getClassLoader();
+                    }
+                });
+            }
+            return Class.forName(className, false, classLoader);
         } catch (ModuleLoadException e) {
             final InvalidClassException ce = new InvalidClassException(className, "Module load failed");
             ce.initCause(e);
@@ -112,7 +138,16 @@ public final class ModularClassResolver implements ClassResolver {
         final String name = (String) unmarshaller.readObject();
         final ClassLoader classLoader;
         if (name == null) {
-            classLoader = Module.class.getClassLoader();
+            if (System.getSecurityManager() == null) {
+                classLoader = Module.class.getClassLoader();
+            } else {
+                classLoader = AccessController.doPrivileged(new PrivilegedAction<ClassLoader>() {
+                    @Override
+                    public ClassLoader run() {
+                        return Module.class.getClassLoader();
+                    }
+                });
+            }
         } else {
             final String slot = (String) unmarshaller.readObject();
             final ModuleIdentifier identifier = ModuleIdentifier.create(name, slot);

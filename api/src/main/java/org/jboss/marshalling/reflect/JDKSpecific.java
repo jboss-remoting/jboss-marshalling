e62e127fac12dae958083c9626fe3606335f49d6
Richard Opálka <opalka.richard@gmail.com>
2023-08-16 18:58:02 +0200

    [JBMAR-253] Minimal supported JDK version is 11 since now on

--- api/src/main/java/org/jboss/marshalling/reflect/JDKSpecific.java
+++ api/src/main/java/org/jboss/marshalling/reflect/JDKSpecific.java
@@ -27,38 +27,25 @@ import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamException;
+import java.lang.invoke.MethodHandle;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-
-import org.jboss.marshalling._private.GetReflectionFactoryAction;
-import org.jboss.marshalling._private.SetAccessibleAction;
+import java.lang.reflect.UndeclaredThrowableException;
+import java.security.PrivilegedAction;
 import sun.reflect.ReflectionFactory;
 
 /**
- * JDK-specific classes which are replaced for different JDK major versions. This one is for Java 8 only.
+ * JDK-specific classes which are replaced for different JDK major versions.  This one is for Java 9 only.
  *
  * @author <a href="mailto:ropalka@redhat.com">Richard Opalka</a>
  */
 final class JDKSpecific {
-    private JDKSpecific() {}
 
-    static final SerializableClassRegistry REGISTRY = SerializableClassRegistry.getInstanceUnchecked();
-
-    private static final ReflectionFactory reflectionFactory = getSecurityManager() == null ? getReflectionFactory() : doPrivileged(GetReflectionFactoryAction.INSTANCE);
+    private static final ReflectionFactory reflectionFactory = getSecurityManager() == null ? getReflectionFactory() : doPrivileged(new PrivilegedAction<ReflectionFactory>() {
+        public ReflectionFactory run() { return ReflectionFactory.getReflectionFactory(); }
+    });
 
     static Constructor<?> newConstructorForSerialization(Class<?> classToInstantiate, Constructor<?> constructorToCall) {
-        final Constructor<?> serCtor = reflectionFactory.newConstructorForSerialization(classToInstantiate, constructorToCall);
-        if (! serCtor.isAccessible()) {
-            // older JDK 8...
-            if (getSecurityManager() == null) {
-                serCtor.setAccessible(true);
-            } else {
-                doPrivileged(new SetAccessibleAction(serCtor));
-            }
-        }
-        return serCtor;
+        return reflectionFactory.newConstructorForSerialization(classToInstantiate, constructorToCall);
     }
 
     /**
@@ -87,6 +74,7 @@ final class JDKSpecific {
      * @param name The record component name
      * @param type The record component class type
      * @return The record component for this record object
+     * @throws java.io.IOException Some error
      */
     static Object getRecordComponentValue(Object recordObject, String name, Class<?> type) {
         throw new UnsupportedOperationException("Records not supported in this version of java");
@@ -99,101 +87,41 @@ final class JDKSpecific {
      * @param fields The fields of the record class
      * @param args The arguments for the constructor
      * @return The instantiated object
+     * @throws java.io.IOException
      */
     static Object invokeRecordCanonicalConstructor(Class<?> recordType, SerializableField[] fields, Object[] args) {
         throw new UnsupportedOperationException("Records not supported in this version of java");
     }
 
     static final class SerMethods {
-        private final Method readObject;
-        private final Method readObjectNoData;
-        private final Method writeObject;
-        private final Method readResolve;
-        private final Method writeReplace;
+        private final MethodHandle readObject;
+        private final MethodHandle readObjectNoData;
+        private final MethodHandle writeObject;
+        private final MethodHandle readResolve;
+        private final MethodHandle writeReplace;
         private final Constructor<?> noArgConstructor;
         private final Constructor<?> objectInputConstructor;
 
         SerMethods(Class<?> clazz) {
-            Method writeObject = null;
-            Method readObject = null;
-            Method readObjectNoData = null;
-            Method writeReplace = null;
-            Method readResolve = null;
-            for (Method method : clazz.getDeclaredMethods()) {
-                final int modifiers = method.getModifiers();
-                final String methodName = method.getName();
-                final Class<?> methodReturnType = method.getReturnType();
-                if (! Modifier.isStatic(modifiers)) {
-                    if (Modifier.isPrivate(modifiers) && methodReturnType == void.class) {
-                        if (methodName.equals("writeObject")) {
-                            final Class<?>[] parameterTypes = method.getParameterTypes();
-                            if (parameterTypes.length == 1 && parameterTypes[0] == ObjectOutputStream.class) {
-                                writeObject = method;
-                                writeObject.setAccessible(true);
-                            }
-                        } else if (methodName.equals("readObject")) {
-                            final Class<?>[] parameterTypes = method.getParameterTypes();
-                            if (parameterTypes.length == 1 && parameterTypes[0] == ObjectInputStream.class) {
-                                readObject = method;
-                                readObject.setAccessible(true);
-                            }
-                        } else if (methodName.equals("readObjectNoData")) {
-                            final Class<?>[] parameterTypes = method.getParameterTypes();
-                            if (parameterTypes.length == 0) {
-                                readObjectNoData = method;
-                                readObjectNoData.setAccessible(true);
-                            }
-                        }
-                    } else if (methodReturnType == Object.class) {
-                        // inheritable
-                        if (methodName.equals("writeReplace")) {
-                            final Class<?>[] parameterTypes = method.getParameterTypes();
-                            if (parameterTypes.length == 0) {
-                                writeReplace = method;
-                                writeReplace.setAccessible(true);
-                            }
-                        } else if (methodName.equals("readResolve")) {
-                            final Class<?>[] parameterTypes = method.getParameterTypes();
-                            if (parameterTypes.length == 0) {
-                                readResolve = method;
-                                readResolve.setAccessible(true);
-                            }
-                        }
-                    }
-                }
-            }
-            if (readResolve == null || writeReplace == null) {
-                final Class<?> superclass = clazz.getSuperclass();
-                if (superclass != null) {
-                    final SerializableClass superInfo = REGISTRY.lookup(superclass);
-                    final Method otherReadResolve = superInfo.getSerMethods().readResolve;
-                    if (readResolve == null && otherReadResolve != null && ! Modifier.isPrivate(otherReadResolve.getModifiers())) {
-                        readResolve = otherReadResolve;
-                    }
-                    final Method otherWriteReplace = superInfo.getSerMethods().writeReplace;
-                    if (writeReplace == null && otherWriteReplace != null && ! Modifier.isPrivate(otherWriteReplace.getModifiers())) {
-                        writeReplace = otherWriteReplace;
-                    }
-                }
-            }
-            this.readObject = readObject;
-            this.readObjectNoData = readObjectNoData;
-            this.writeObject = writeObject;
-            this.readResolve = readResolve;
-            this.writeReplace = writeReplace;
+            readObject = reflectionFactory.readObjectForSerialization(clazz);
+            readObjectNoData = reflectionFactory.readObjectNoDataForSerialization(clazz);
+            writeObject = reflectionFactory.writeObjectForSerialization(clazz);
+            readResolve = reflectionFactory.readResolveForSerialization(clazz);
+            writeReplace = reflectionFactory.writeReplaceForSerialization(clazz);
+            Constructor<?> ctor;
             Constructor<?> noArgConstructor = null;
-            Constructor<?> objectInputConstructor = null;
-            for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
-                final Class<?>[] parameterTypes = constructor.getParameterTypes();
-                if (parameterTypes.length == 0) {
-                    noArgConstructor = constructor;
-                    noArgConstructor.setAccessible(true);
-                } else if (parameterTypes.length == 1 && parameterTypes[0] == ObjectInput.class) {
-                    objectInputConstructor = constructor;
-                    objectInputConstructor.setAccessible(true);
-                }
+            try {
+                ctor = clazz.getDeclaredConstructor();
+                noArgConstructor = reflectionFactory.newConstructorForSerialization(clazz, ctor);
+            } catch (NoSuchMethodException ignored) {
             }
             this.noArgConstructor = noArgConstructor;
+            Constructor<?> objectInputConstructor = null;
+            try {
+                ctor = clazz.getDeclaredConstructor(ObjectInput.class);
+                objectInputConstructor = reflectionFactory.newConstructorForSerialization(clazz, ctor);
+            } catch (NoSuchMethodException ignored) {
+            }
             this.objectInputConstructor = objectInputConstructor;
         }
 
@@ -204,19 +132,10 @@ final class JDKSpecific {
         void callWriteObject(Object object, ObjectOutputStream outputStream) throws IOException {
             try {
                 writeObject.invoke(object, outputStream);
-            } catch (InvocationTargetException e) {
-                final Throwable te = e.getTargetException();
-                if (te instanceof IOException) {
-                    throw (IOException)te;
-                } else if (te instanceof RuntimeException) {
-                    throw (RuntimeException)te;
-                } else if (te instanceof Error) {
-                    throw (Error)te;
-                } else {
-                    throw new IllegalStateException("Unexpected exception", te);
-                }
-            } catch (IllegalAccessException e) {
-                throw new IllegalStateException("Method is unexpectedly inaccessible");
+            } catch (IOException | RuntimeException | Error e) {
+                throw e;
+            } catch (Throwable e) {
+                throw new UndeclaredThrowableException(e);
             }
         }
 
@@ -227,21 +146,10 @@ final class JDKSpecific {
         void callReadObject(Object object, ObjectInputStream inputStream) throws IOException, ClassNotFoundException {
             try {
                 readObject.invoke(object, inputStream);
-            } catch (InvocationTargetException e) {
-                final Throwable te = e.getTargetException();
-                if (te instanceof IOException) {
-                    throw (IOException)te;
-                } else if (te instanceof ClassNotFoundException) {
-                    throw (ClassNotFoundException)te;
-                } else if (te instanceof RuntimeException) {
-                    throw (RuntimeException)te;
-                } else if (te instanceof Error) {
-                    throw (Error)te;
-                } else {
-                    throw new IllegalStateException("Unexpected exception", te);
-                }
-            } catch (IllegalAccessException e) {
-                throw new IllegalStateException("Method is unexpectedly inaccessible");
+            } catch (IOException | ClassNotFoundException | RuntimeException | Error e) {
+                throw e;
+            } catch (Throwable e) {
+                throw new UndeclaredThrowableException(e);
             }
         }
 
@@ -252,19 +160,10 @@ final class JDKSpecific {
         void callReadObjectNoData(Object object) throws ObjectStreamException {
             try {
                 readObjectNoData.invoke(object);
-            } catch (InvocationTargetException e) {
-                final Throwable te = e.getTargetException();
-                if (te instanceof ObjectStreamException) {
-                    throw (ObjectStreamException)te;
-                } else if (te instanceof RuntimeException) {
-                    throw (RuntimeException)te;
-                } else if (te instanceof Error) {
-                    throw (Error)te;
-                } else {
-                    throw new IllegalStateException("Unexpected exception", te);
-                }
-            } catch (IllegalAccessException e) {
-                throw new IllegalStateException("Method is unexpectedly inaccessible");
+            } catch (ObjectStreamException | RuntimeException | Error e) {
+                throw e;
+            } catch (Throwable e) {
+                throw new UndeclaredThrowableException(e);
             }
         }
 
@@ -275,19 +174,10 @@ final class JDKSpecific {
         Object callWriteReplace(Object object) throws ObjectStreamException {
             try {
                 return writeReplace.invoke(object);
-            } catch (InvocationTargetException e) {
-                final Throwable te = e.getTargetException();
-                if (te instanceof ObjectStreamException) {
-                    throw (ObjectStreamException)te;
-                } else if (te instanceof RuntimeException) {
-                    throw (RuntimeException)te;
-                } else if (te instanceof Error) {
-                    throw (Error)te;
-                } else {
-                    throw new IllegalStateException("Unexpected exception", te);
-                }
-            } catch (IllegalAccessException e) {
-                throw new IllegalStateException("Method is unexpectedly inaccessible");
+            } catch (ObjectStreamException | RuntimeException | Error e) {
+                throw e;
+            } catch (Throwable e) {
+                throw new UndeclaredThrowableException(e);
             }
         }
 
@@ -298,19 +188,10 @@ final class JDKSpecific {
         Object callReadResolve(Object object) throws ObjectStreamException {
             try {
                 return readResolve.invoke(object);
-            } catch (InvocationTargetException e) {
-                final Throwable te = e.getTargetException();
-                if (te instanceof ObjectStreamException) {
-                    throw (ObjectStreamException)te;
-                } else if (te instanceof RuntimeException) {
-                    throw (RuntimeException)te;
-                } else if (te instanceof Error) {
-                    throw (Error)te;
-                } else {
-                    throw new IllegalStateException("Unexpected exception", te);
-                }
-            } catch (IllegalAccessException e) {
-                throw new IllegalStateException("Method is unexpectedly inaccessible");
+            } catch (ObjectStreamException | RuntimeException | Error e) {
+                throw e;
+            } catch (Throwable e) {
+                throw new UndeclaredThrowableException(e);
             }
         }
 
